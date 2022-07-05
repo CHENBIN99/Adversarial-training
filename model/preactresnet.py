a8b021df955ef769b15ca76d46d4d0e64948469e
CHENBIN <30487978+CHENBIN99@users.noreply.github.com>
2022-07-05 22:10:35 +0800

    fix bug

--- model/preactresnet.py
+++ model/preactresnet.py
@@ -65,7 +65,7 @@ class PreActResNet(nn.Module):
     def __init__(self, block, num_blocks, num_classes=10, stride=1):
         super(PreActResNet, self).__init__()
         self.in_planes = 64
-        self.conv1 = nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=stride, bias=False)
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=3, stride=stride, padding=1, bias=False)
         self.layer1 = self._make_layer(block, 64, num_blocks[0], stride=1)
         self.layer2 = self._make_layer(block, 128, num_blocks[1], stride=2)
         self.layer3 = self._make_layer(block, 256, num_blocks[2], stride=2)
